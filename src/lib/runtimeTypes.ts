f5619b0438e41235bc78b8f60d9a507b8cd4e8ff
dosordie <14365099+dosordie@users.noreply.github.com>
2026-07-08 14:36:10 +0200

    Extract adapter runtime types

---
+++ src/lib/runtimeTypes.ts
@@ -0,0 +1,27 @@
+import { HomeConnectClient } from "./client";
+import { StateMapper } from "./stateMapper";
+import { ApplianceProfile, ConfiguredDevice } from "./types";
+
+export interface RunningDevice {
+  baseId: string;
+  config: ConfiguredDevice;
+  profile: ApplianceProfile;
+  mapper: StateMapper;
+  client?: HomeConnectClient;
+  reconnectTimer?: NodeJS.Timeout;
+  reconnecting: boolean;
+  reconnectFailures: number;
+  writableUids: Set<string>;
+  blockedCommands: string[];
+  stateValuesByFeature: Map<string, ioBroker.StateValue>;
+}
+
+export type WritableStateKind = "value" | "command" | "startProgram" | "startProgramWithOptions";
+
+export interface WritableState {
+  deviceHaId: string;
+  uid: number;
+  featureName: string;
+  kind: WritableStateKind;
+  stateId: string;
+}

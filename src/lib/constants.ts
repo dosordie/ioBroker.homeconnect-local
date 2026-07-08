37c126660d0b0a4fea18cabb46e20af6684f9b5f
dosordie <14365099+dosordie@users.noreply.github.com>
2026-07-08 14:36:03 +0200

    Extract adapter constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,19 @@
+export const POWER_STATE_UID = "021B";
+export const POWER_STATE_UID_NUMBER = 0x021b;
+export const POWER_STATE_OFF = 1;
+export const POWER_STATE_ON = 2;
+
+export const SELECTED_PROGRAM_FEATURE = "BSH.Common.Root.SelectedProgram";
+export const ACTIVE_PROGRAM_FEATURE = "BSH.Common.Root.ActiveProgram";
+export const START_IN_FEATURE = "BSH.Common.Option.StartInRelative";
+export const FINISH_IN_FEATURE = "BSH.Common.Option.FinishInRelative";
+
+export const DANGEROUS_COMMAND_MARKERS = [
+  "FactoryReset",
+  "NetworkReset",
+  "DeactivateWiFi",
+  "AllowSoftwareUpdate",
+  "AllowSoftwareDownload",
+  "SoftwareUpdate",
+  "SoftwareDownload",
+];
